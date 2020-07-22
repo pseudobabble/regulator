bd809f49167b3e030fc4871b1845f52fa5942a9c
pseudobabble <harryjohnson500@gmail.com>
2020-07-22 08:41:48 +0100

    - moar tests

--- tests/RuleChainTest.py
+++ tests/RuleChainTest.py
@@ -1,25 +1,63 @@
 #!/usr/bin/env python3
 
-import unittest
+from typing import Any
 
+from pytest import mark
+
+from regulator import RuleChain
 from regulator.Rule import Rule
 
 
-class TestRuleChain(unittest.TestCase):
+class TestRuleChain:
 
-    def test_run(self):
-        pass
+    def test__create(self):
+        rules = [Rule()]
+        chain = self._get_rule_chain(rules)
+        assert chain.rules == rules
+
+    @mark.parametrize('rule_type, priority, function, item', [
+        [str, 1, lambda x: x*2, 'a'],
+        [bool, 1, lambda x: x, True],
+        [int, 1, lambda x: x, 1],
+        [float, 1, lambda x: x, 1.0],
+        [list, 1, lambda x: x, []],
+        [dict, 1, lambda x: x, {}],
+    ])
+    def test_run_chain(self, rule_type, priority, function, item):
+        rules = [self._get_concrete_rule(rule_type, priority, function)]
+        rule_chain = self._get_rule_chain(rules)
+        modified_item = rule_chain.run(item)
+        assert modified_item == function(item)
+
 
     def test__higher_priority_is_run_first(self):
-        first_rule = self._get_concrete_rule(str, 1, lambda x: x*2)
+        first_rule = self._get_concrete_rule(str, 1, lambda x: x.replace('X', 'U'))
         second_rule = self._get_concrete_rule(str, 2, lambda x: x*3)
+        rule_chain = self._get_rule_chain([first_rule, second_rule])
 
         item = 'X'
-        item = first_rule.run(item)
-        assert item == 'XX'
+        modified_item = rule_chain.run(item)
+        assert modified_item == 'UUU'
+        assert modified_item != 'XXX'
 
-        item = second_rule.run(item)
-        assert item == 'XXXXXX'
+    def _get_rule_chain(self, rules_list):
+        class MyRuleChain(RuleChain):
+            rules = rules_list
 
+        rule_chain = MyRuleChain()
 
+        return rule_chain
 
+    def _get_concrete_rule(self, type, priority, function):
+        class ConcreteRule(Rule):
+            rule_type = type
+
+            def apply(self, item, *args, **kwargs) -> Any:
+                return function(item)
+
+            def get_priority(self) -> int:
+                return priority
+
+        concrete_rule = ConcreteRule()
+
+        return concrete_rule
--- tests/RuleTest.py
+++ tests/RuleTest.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import pytest
-from pytest import mark
+from pytest import mark, raises
 from typing import Any
 
 from regulator.Rule import Rule
@@ -9,13 +9,25 @@ from regulator.Rule import Rule
 
 class TestRule:
 
+    def test__create(self):
+        rule = self._get_concrete_rule(str, 1, lambda x: x*2)
+        assert rule.rule_type == str
+        assert rule.get_priority() == 1
+
+    def test__exceptions_raised_calling_abstract(self):
+        rule = Rule()
+        with raises(NotImplementedError):
+            rule.apply('a')
+        with raises(NotImplementedError):
+            rule.get_priority()
+
     @mark.parametrize('valid_type, invalid_items, function', [
         (str, (True, 1, 1.0, [], {}), lambda x: x*2),
         (bool, ('hello', 1, 1.0, [], {}), lambda x: not x),
         (int, (True, 'hello', 1.0, [], {}), lambda x: x*2),
         (float, (True, 1, 'hello', [], {}), lambda x: x*2),
         (list, (True, 1, 1.0, 'hello', {}), lambda x: x.append(1)),
-        (dict, (True, 1, 'hello', [], 'hello'), lambda x: x.set('a', 'a')),
+        (dict, (True, 1, 1.0, [], 'hello'), lambda x: x.set('a', 'a')),
     ])
     def test__no_change_on_invalid_type(self, valid_type, invalid_items, function):
         rule = self._get_concrete_rule(valid_type, 1, function)
